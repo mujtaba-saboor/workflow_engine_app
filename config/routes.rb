a2d847227f13720a966f5b417093711e458d6b90
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-07 17:53:09 +0500

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login'}
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
