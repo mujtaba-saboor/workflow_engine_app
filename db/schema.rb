bc8cd23f37346371a264fc2e4fa591b3eed65ce9
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-07 18:38:02 +0500

    Company table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_121405) do
+ActiveRecord::Schema.define(version: 2020_09_07_133359) do
+
+  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "domain", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", default: "", null: false
