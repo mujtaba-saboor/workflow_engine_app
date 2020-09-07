acedd335d062da626329851783223c9faec56453
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-07 18:41:38 +0500

    Added enums and basic validations for Issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Issue < ApplicationRecord
+  has_many :comments
+
+  enum type: %i[bug issue]
+  enum priority: %i[low high]
+  enum status: %i[open in_progress resolved closed]
+
+  validates :title, :type, :priority, :status, presence: true
 end
