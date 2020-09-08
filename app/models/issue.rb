214a28b27abedbe0cb44160034d7602476feeab2
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-08 12:38:40 +0500

    Added aasm to Issue model

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
 class Issue < ApplicationRecord
+  include AASM
+
   has_many :comments, as: :commentable
 
-  enum type: %i[bug issue]
-  enum priority: %i[low high]
-  enum status: %i[open in_progress resolved closed]
+  enum issue_type: %i[bug issue], _prefix: true
+  enum priority: %i[low high], _prefix: true
+  enum status: %i[open in_progress resolved closed], _scopes: false, _prefix: true
 
   validates :title, :type, :priority, :status, presence: true
+
+  aasm column: :status, enum: true do
+    state :open, initial: true
+    state :in_progress, :resolved, :closed
+    after_all_transitions :transition_callback
+
+    event :reopen do
+      transitions from: :closed, to: :open
+    end
+  end
+
+  def transition_callback
+    puts "Current event: #{aasm.current_event}"
+    puts "Transition Callback: from #{aasm.from_state} to #{aasm.to_state}"
+  end
 end
