df02a45c282db2699ed19c3a2182d9b8a460e3fe
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-08 18:09:40 +0500

    Resolved extra lines issues

--- db/migrate/20200907124126_create_comments.rb
+++ db/migrate/20200907124126_create_comments.rb
@@ -2,9 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.references :commentable, polymorphic: true
-
       t.text :body
-
       t.timestamps
     end
   end
