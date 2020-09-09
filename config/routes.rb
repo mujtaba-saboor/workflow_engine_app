1c23071a0afa62dbc80a45b913d6c14ce35d6904
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-09 17:18:24 +0500

    Issue routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
     resources :users
   end
   devise_for :users
+
+  resources :projects do
+    resources :issues
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
