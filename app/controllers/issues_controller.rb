05ef66bc251eeb0c1db380aec66e88b40d1db7f9
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-10 17:12:54 +0500

    Completed CRUD operations for Issue model

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,9 +5,56 @@ class IssuesController < ApplicationController
     @comments = Comment.where(commentable: @issue)
   end
 
-  # private
+  def new
+    @project = Project.find(params[:project_id])
+    @issue = Issue.new
+    @valid_assignees = @project.company.users
+  end
 
-  # def issue_params
-  #   params.require(:issue).permit(:title, :description, :issue_type, :priority, :status, :project_id, :creator_id, :assignee_id, :company_id)
-  # end
+  def create
+    @project = Project.find(params[:project_id])
+    @issue = Issue.new(issue_params)
+
+    @issue.creator = current_user
+    @issue.company = current_user.company
+    @issue.project = @project
+
+    if @issue.save
+      redirect_to project_issue_path(@project, @issue)
+    else
+      @valid_assignees = @project.company.users
+      render 'new'
+    end
+  end
+
+  def edit
+    @issue = Issue.find_by!(id: params[:id], project_id: params[:project_id])
+    @project = @issue.project
+    @issue = Issue.find(params[:id])
+    @valid_assignees = @project.company.users
+  end
+
+  def update
+    @issue = Issue.find_by!(id: params[:id], project_id: params[:project_id])
+    @project = @issue.project
+
+    if @issue.update(issue_params)
+      redirect_to project_issue_path(@project, @issue)
+    else
+      @valid_assignees = @project.company.users
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @issue = Issue.find_by!(id: params[:id], project_id: params[:project_id])
+    @issue.destroy
+    redirect_to project_path(params[:project_id])
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:title, :description, :issue_type, :priority, :status, :assignee_id)
+  end
 end
