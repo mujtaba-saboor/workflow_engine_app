38c3a2508fdd32cb6b7dbedaf689adce4443ecff
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-14 17:38:14 +0500

    Added a guard clause instead of long if statement

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,18 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.present?
-      can :create, Comment
-      can :manage, Comment, user_id: user.id, company_id: user.company_id
+    return if user.blank?
 
-      can :read, Project, company_id: user.company_id
+    can :create, Comment
+    can :manage, Comment, user_id: user.id, company_id: user.company_id
 
-      can :read, Issue, company_id: user.company_id
-      can :create, Issue, company_id: user.company_id
-      can :destroy, Issue, creator_id: user.id, company_id: user.company_id
-      can :update, Issue do |issue|
-        (issue.assignee_id == user.id || issue.creator_id == user.id) && issue.company_id == user.company_id
-      end
+    can :read, Project, company_id: user.company_id
+
+    can :read, Issue, company_id: user.company_id
+    can :create, Issue, company_id: user.company_id
+    can :destroy, Issue, creator_id: user.id, company_id: user.company_id
+    can :update, Issue do |issue|
+      (issue.assignee_id == user.id || issue.creator_id == user.id) && issue.company_id == user.company_id
     end
   end
 end
