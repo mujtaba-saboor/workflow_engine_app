7729fc3e88be7f80d5b87da9a03f09f4fddc67b4
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-14 18:16:50 +0500

    domain and company id set to NOT NULL

--- db/migrate/20200907133359_create_companies.rb
+++ db/migrate/20200907133359_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
       t.string :name,   null: false, default: ""
-      t.string :domain, null: true, default: ""
+      t.string :domain, null: false, default: ""
       t.timestamps
     end
 
