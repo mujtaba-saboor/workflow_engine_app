5cf19c3cea64150af7da601a22086991391b2aa8
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-14 18:52:00 +0500

    Added helpers for adding bootstrap classes

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -14,4 +14,25 @@ module IssuesHelper
   def select_options_for_enum(enum_hash)
     enum_hash.collect { |key, _| [key.humanize, key] }
   end
+
+  def bootstrap_color_string_for_status(status)
+    case status
+    when 'open'
+      'primary'
+    when 'in_progress'
+      'info'
+    when 'resolved'
+      'warning'
+    when 'closed'
+      'success'
+    end
+  end
+
+  def bootstrap_color_string_for_priority(priority)
+    priority == 'high' ? 'danger' : 'warning'
+  end
+
+  def bootstrap_color_string_for_issue_type(issue_type)
+    issue_type == 'bug' ? 'danger' : 'warning'
+  end
 end
