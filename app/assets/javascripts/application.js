1055449d11976709dd01fdea7ce42fad95fa5f82
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-15 11:20:41 +0500

    Sub domain field rectified

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 function make_domain() {
   var x = document.getElementById("company_name_id").value;
   document.getElementById("domain_place").innerHTML = "Your portal will be live at: https://" + x + ".workengine.com";
-  document.getElementById("domain_id").value = "https://" + x + ".workengine.com"
+  document.getElementById("domain_id").value = x
 }
