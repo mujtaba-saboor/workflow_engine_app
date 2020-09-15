cbc0b64bd5c523f8176564c592fa72419b3b9371
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-15 14:03:30 +0500

    Validation Added in domain column

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 function make_domain() {
   var x = document.getElementById("company_name_id").value;
+  x = x.replace(/\W/g, '')
   document.getElementById("domain_place").innerHTML = "Your portal will be live at: https://" + x + ".workengine.com";
   document.getElementById("domain_id").value = x
 }
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@ class Company < ApplicationRecord
   has_many :users
   has_many :projects
   has_many :teams
+
+  validates :domain, format: { with: /\A[a-zA-Z0-9]+\z/,
+    message: "only allows letters and numbers" }
 end
