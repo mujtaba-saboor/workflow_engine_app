cc815e5e540c8aed73ad0ed5e691f209fbbfcd5c
Haider Shahid <haider.shahid@7vals.com>
2020-09-16 18:34:22 +0500

    UI added with dashboard

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def get_team_percentage
+    total_projects = Project.all.count
+    team_projects = Project.where(project_category: 'TEAM').count
+    team_projects != 0 ? team_projects = (team_projects * 100)/total_projects : 0
+  end
+  def get_individual_percentage
+    total_projects = Project.all.count
+    individual_projects = Project.where(project_category: 'INDIVIDUAL').count
+    individual_projects != 0 ? individual_projects = (individual_projects * 100)/total_projects : 0
+  end
 end
