e4579bcf89c8dec3a9503a7475d9de00beda3030
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-16 22:32:36 +0500

    Move onChange

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 function make_subdomain() {
   var x = document.getElementById("company_name_id").value;
-  x = x.replace(/\W/g, '')
+  x = x.replace(/\W/g, '');
   document.getElementById("domain_place").innerHTML = "Your portal will be live at: https://" + x + ".workengine.com";
-  document.getElementById("subdomain_id").value = x
+  document.getElementById("subdomain_id").value = x;
 }
