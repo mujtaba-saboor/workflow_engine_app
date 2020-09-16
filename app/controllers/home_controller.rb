480123b252b2bf466c764849820e5970e4f79c01
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-16 19:19:14 +0500

    Added some path blocks for base paths and subdomain paths

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  skip_around_action :scope_current_company
   def index
     flash.now[:notif] = "Welcome to Workflow Engine"
   end
