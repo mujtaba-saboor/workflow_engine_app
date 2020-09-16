24a49646d635123a1896eb0a2433284ffd8f6eec
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-16 18:51:12 +0500

    Domain changed to subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, company_attributes: [:name, :domain]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, company_attributes: [:name, :subdomain]])
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,6 @@ class Company < ApplicationRecord
   has_many :projects
   has_many :teams
 
-  validates :domain, format: { with: /\A[a-zA-Z0-9]+\z/,
+  validates :subdomain, format: { with: /\A[a-zA-Z0-9]+\z/,
     message: "only allows letters and numbers" }
 end
