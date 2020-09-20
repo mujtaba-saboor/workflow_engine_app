99c2cf80ab8b658c84a7be626ad618c83b49835b
Haider Shahid <haider.shahid@7vals.com>
2020-09-20 15:03:34 +0500

    Nav bar updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def get_top_nav_links
+    {t('shared.home')=> root_path, t('shared.projects') => projects_path, t('shared.teams') => teams_path}
+  end
+
+  def get_sidebar_links
+    {t('shared.home')=> root_path, t('shared.projects') => projects_path, t('shared.teams') => teams_path}    
+  end
+  
+  def is_active(controller)       
+    params[:controller] == controller ? "active" : nil        
+  end
 end
