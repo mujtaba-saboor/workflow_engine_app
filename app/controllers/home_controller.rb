402a55b1a61918117aba7450a62fa70c3e437efc
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-21 19:09:10 +0500

    Changed the error message format and its display mechanism

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,25 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     flash.now[:notice] = t('home.welcome_message')
-    respond_to :html
+    respond_to do |format|
+      format.html
+    end
   end
 
   # get '/users/sign_in', to: 'home#sign_in', constraints: { subdomain: '' }
   def sign_in
-    respond_to :html
+    respond_to do |format|
+      format.html
+    end
   end
 
   # get '/user/companies', to: 'home#user_companies', as: user_companies
   def user_companies
     email = params[:email]
     @companies = Company.joins(:users).where('users.email = ?', email)
-    respond_to :js
+    flash.now[:error] = t('home.no_associated_companies_message', email: email) if @companies.empty?
+    respond_to do |format|
+      format.js
+    end
   end
 end
