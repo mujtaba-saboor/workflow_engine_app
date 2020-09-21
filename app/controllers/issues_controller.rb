e3ecec83fb067518d2b7939d5120b5771dbcdd31
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-21 13:06:31 +0500

    Documents Added with Issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,7 +1,26 @@
 class IssuesController < ApplicationController
 
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    @issue = Issue.new(issues_parameters)
+    @issue.project_id = 1
+    @issue.creator_id = 1
+    @issue.company_id = 1
+    if @issue.save
+      flash[:alert] = 'Issue was successfully created.'
+      redirect_to action: 'new'
+    else
+      flash[:alert] = 'Issue was not created.'
+      redirect_to action: 'new'
+    end
+
+  end
+
   private
     def issues_parameters
-      params.require(:issue).permit(documents: [])
+      params.require(:issue).permit(:title, :description, :assignee_id, :status, :priority, :issue_type, documents: [])
     end
 end
