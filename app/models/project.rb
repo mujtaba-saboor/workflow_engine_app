af9351c3b0e769f84136c8d43aced7f455823760
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-22 14:20:12 +0500

    Changed a query

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,10 +11,15 @@ class Project < ApplicationRecord
 
   def valid_assignees
     if project_category == 'TEAM'
-      User.joins(%(INNER JOIN `team_users`
-      ON `team_users`.`user_id` = `users`.`id` and `team_users`.`company_id` = #{company_id}
-      INNER JOIN `project_teams`
-      ON `project_teams`.`team_id` = `team_users`.`team_id` and `project_teams`.`project_id` = #{id})).distinct
+      # User.joins(%(INNER JOIN `team_users`
+      # ON `team_users`.`user_id` = `users`.`id` and `team_users`.`company_id` = #{company_id}
+      # INNER JOIN `project_teams`
+      # ON `project_teams`.`team_id` = `team_users`.`team_id` and `project_teams`.`project_id` = #{id})).distinct
+
+      # This query has an extra inner join as compared to one above however it offers more readability, so it is used.
+      # There is an extra inner join as joins works using associations and in above query we want to join two middle
+      # tables so rails join them via the intermediate table
+      teams.joins(:users).select('users.*').distinct
     else
       company.users
     end
