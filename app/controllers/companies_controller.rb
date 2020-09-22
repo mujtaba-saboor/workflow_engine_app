578149d756c67c03c42df65b04eaad121aadd232
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-22 10:35:23 +0500

    Separated routes accessible with and without subdomains using block; also some minor fixes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,5 +5,8 @@ class CompaniesController < ApplicationController
     @projects = Project.all
     @issues = Issue.all
     flash.now[:notice] = t('companies.welcome_message', company_name: @company.name)
+    respond_to do |format|
+      format.html
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,32 @@
 Rails.application.routes.draw do
-  get '', to: 'companies#index', constraints: { subdomain: /.+/ }
-  root 'home#index'
-
-  resources :projects do
-    member do
-      get 'project_users'
-      get 'new_team_for_project'
-      patch 'add_team_to_project'
-      delete 'remove_team_from_project'
-      get 'new_user_for_project'
-      patch 'add_user_to_project'
-      delete 'remove_user_from_project'
+  # Routes accessible with subdomain
+  constraints(subdomain: /.+/) do
+    resources :projects do
+      member do
+        get 'project_users'
+        get 'new_team_for_project'
+        patch 'add_team_to_project'
+        delete 'remove_team_from_project'
+        get 'new_user_for_project'
+        patch 'add_user_to_project'
+        delete 'remove_user_from_project'
+      end
     end
-  end
-  resources :teams do
-    member do
-      get 'new_user_for_team'
-      patch 'add_user_to_team'
-      delete 'remove_user_from_team'
+    resources :teams do
+      member do
+        get 'new_user_for_team'
+        patch 'add_user_to_team'
+        delete 'remove_user_from_team'
+      end
     end
+    get '', to: 'companies#index'
+    devise_for :users
   end
 
-  get '/users/sign_in', to: 'home#sign_in', constraints: { subdomain: '' }
-  post '/user/companies', to: 'home#user_companies', as: 'user_companies'
-  devise_for :users
-
-  # TODO: Make blocks for routes with subdomain and routes without subdomains
+  # Routes accessible without subdomain
+  constraints(subdomain: '') do
+    get '/users/sign_in', to: 'home#sign_in'
+    post '/user/companies', to: 'home#user_companies', as: 'user_companies'
+    root 'home#index'
+  end
 end
