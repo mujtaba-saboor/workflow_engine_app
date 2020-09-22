8a77797eb51f58e74e67802392b5682244639fa8
Haider Shahid <haider.shahid@7vals.com>
2020-09-22 19:29:42 +0500

    Some UI Fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ApplicationRecord
   def all_projects
     company = Company.first
     all_individual_projects = projects.ids
-    all_team_projects = company.projects.joins(:teams).where(teams: { id: self.teams.pluck(:id) }).pluck(:id)
+    all_team_projects = company.projects.joins(:teams).where(teams: { id: teams.pluck(:id) }).pluck(:id)
     company.projects.where(id: all_individual_projects | all_team_projects)
   end
 
   def get_project_count
-    if(self.role.eql? ROLES[0])
+    if role.eql? ROLES[0]
       all_projects.count
     else
       Project.all.count
@@ -33,14 +33,26 @@ class User < ApplicationRecord
 
   # Specifically made for STAFF user
   def get_team_project_count
-    Project.where(project_category: Project::PROJECT_CATEGORIES[0]).where(id: self.teams.pluck(:id)).count
+    Project.where(project_category: Project::PROJECT_CATEGORIES[0]).where(id: teams.pluck(:id)).count
   end
 
   def get_team_count
-    if(self.role.eql? ROLES[0])
-      self.teams.count
+    if role.eql? ROLES[0]
+      teams.count
     else
       Team.all.count
     end
+  end
+
+  def staff?
+    role.eql? User::ROLES[0]
+  end
+
+  def admin?
+    role.eql? User::ROLES[1]
+  end
+
+  def owner?
+    role.eql? User::ROLES[2]
   end
 end
