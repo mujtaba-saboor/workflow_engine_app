a4caa1fe3469458618452da41f66be09cccb11a2
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-22 17:26:52 +0500

    Internationalization Added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ include Pagy::Backend
 class UsersController < ApplicationController
   def index
     @pagy, @users = pagy(User.where(params[:company_id]),  items: 5)
-    add_breadcrumb "Show_Users", :company_users_path  
+    add_breadcrumb t('shared.users'), :company_users_path  
   end
 end
