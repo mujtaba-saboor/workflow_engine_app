943c12d7423a664f03736e5cff090af9e502fb9f
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-22 14:19:42 +0500

    Role corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, company_attributes: [:name, :subdomain]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role, company_attributes: [:name, :subdomain]])
   end
 end
