04015925c3d4ec76136405cf70c5d29730a3f33f
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-23 18:32:32 +0500

    Move to document Conflicts

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
       can :read, Team, id: user.teams.pluck(:id)
       can :read, Project,  id: user.all_projects.pluck(:id)
       can :project_users, Project
-      can :read, User,  sequence_num: user.company.users.pluck(:id)
+      can :read, User,  id: user.id
     elsif user.admin?
       can :manage, :all
       cannot :destroy, User, role: User::ROLES[2]
