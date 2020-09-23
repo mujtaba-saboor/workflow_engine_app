75234670ea5c4ab8ec31db8019f8d0c0c15007f3
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-23 10:45:16 +0500

    Added load and authorize resource

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 include Pagy::Backend
 class UsersController < ApplicationController
+  load_and_authorize_resource
   def index
+    add_breadcrumb t('shared.users'), :company_users_path
     @pagy, @users = pagy(User.where(params[:company_id]),  items: 5)
-    add_breadcrumb t('shared.users'), :company_users_path  
   end
 end
