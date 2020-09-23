13dfcebe0b57d1dd37ab21fe17fdc28a77a25cbc
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-23 15:11:23 +0500

    Removed unnecessary code

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,7 @@
 class CompaniesController < ApplicationController
   # GET '', subdomain: /.+/
   def index
-    @company = current_company
-    @projects = Project.all
-    @issues = Issue.all
-    flash.now[:notice] = t('companies.welcome_message', company_name: @company.name)
+    flash.now[:notice] = t('companies.welcome_message', company_name: @current_company.name)
     respond_to do |format|
       format.html
     end
