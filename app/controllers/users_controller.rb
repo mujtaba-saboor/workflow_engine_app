f4b02c55fe505d6725b185a044da8082150d91b0
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-24 19:12:24 +0500

    Comments Resolve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ include Pagy::Backend
 class UsersController < ApplicationController
   load_and_authorize_resource find_by: :sequence_num, through: :current_company
   def show
+    add_breadcrumb t('users.username', user_name: current_user.name), :user_path
     respond_to { |format| format.html }
   end
 
   def index
-    add_breadcrumb t('shared.users'), :company_users_path
+    add_breadcrumb t('shared.users'), :users_path
     @pagy, @users = pagy(@users,  items: Company::PAGE_SIZE)
     respond_to { |format| format.html }
   end
