dd28db05a458f0e2810cfc9fa83f0e87e158a14d
Usman Ahmad <usman.ahmad@7vals.com>
2020-09-25 13:00:36 +0500

    Removed extra comments and edited view a bit

--- app/mailers/watcher_mailer.rb
+++ app/mailers/watcher_mailer.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class WatcherMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.watcher_mailer.watching_issue_now.subject
-  #
   def watching_issue_now(user, issue, subdomain)
     send_mail(user, issue, subdomain)
   end
