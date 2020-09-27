bf5cc491f5ab11fb46b7909b884624caa1445f3d
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-27 06:15:50 +0500

    Stuck in an error

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,5 @@
 class InvitesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:confirm_request]
+  skip_before_action :authenticate_user!, only: [:confirm_request, :create_staff_user]
   def new
     @invite = Invite.new
     @token = params[:invite_token] #<-- pulls the value from the url query string
@@ -12,28 +12,47 @@ class InvitesController < ApplicationController
     @invite.recipient.email = @invite.email
     @invite.recipient.role = User::ROLES[0]
     @invite.recipient.company = current_company
-
     if @invite.save
-      InviteMailer.with(invite: @invite, path: confirm_request_path).new_user_invite.deliver #send the invite data to our mailer to deliver the email
+      InviteMailer.with(invite: @invite, path: confirm_request_path(email: @invite.recipient.email, company: current_company)).new_user_invite.deliver #send the invite data to our mailer to deliver the email
       flash[:success] = t('invites.send_successful')
       respond_to do |format|
         format.html { redirect_to new_invite_path }
       end
     else
-      flash[:danger] = t('flash_messages.error', error_msg: @project.errors.full_messages.first)
+      flash[:danger] = t('flash_messages.error', error_msg: @invite.errors.full_messages.first)
     end
   end
 
   def confirm_request
-    binding.pry
+    @email = params[:email]
+    @company = params[:company]
     @user = User.new
   end
 
   def create_staff_user
-      #user will be created here
+    @user = User.new
+    @user.company = Company.all.find(confirm_params[:company_id])
+    @user.name = confirm_params[:name]
+    @user.role = confirm_params[:role]
+    @user.email = confirm_params[:email]
+    @user.password = confirm_params[:password]
+    @user.password_confirmation = confirm_params[:password_confirmation]
+    binding.pry
+    if @user.save
+      flash[:success] = t('users.user_created_successfully')
+      respond_to do |format|
+        format.html { redirect_to '/users/sign_in' }
+      end
+    else
+      flash[:danger] = t('flash_messages.error', error_msg: @user.errors.full_messages.first)
+    end
   end
 
   def invite_params
-    params.require(:invite).permit(:company_id, :email)
+    params.require(:invite).permit(:company, :email)
+  end
+
+  def confirm_params
+    params.require(:user).permit(:name, :company_id, :role, :email, :password, :password_confirmation)
   end
 end
