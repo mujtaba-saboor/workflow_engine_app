65e19633faeda8cb1487258cb8ef6d8575237e51
hammad-Ikhlaq-7vals <hammad.ikhlaq@7vals>
2020-09-28 14:00:15 +0500

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,15 @@
-$(document).ready(function(){
-  $("#company_name_id").change(function(){
+$(document).on('turbolinks:load', function(){
+  $("body").on("change", "#company_name_id", function(){
     var subdomain = $(this).val();
     subdomain = subdomain.replace(/\W/g, '');
     $("#domain_place").html("Your portal will be live at: https://" + subdomain + ".workengine.com");
     $("#subdomain_id").val(subdomain);
   });
-});
 
-$(document).ready(function(){
-        $("#add_file_field_id").change(
-            function(){
-                if ($(this).val()) {
-                    $("#add_file_field_button_id").attr('disabled',false);
-                }
-            }
-            );
-    });
+  $("#add_file_field_id").change(function(){
+        if ($(this).val()) {
+            $("#add_file_field_button_id").attr('disabled',false);
+        }
+    }
+    );
+});
