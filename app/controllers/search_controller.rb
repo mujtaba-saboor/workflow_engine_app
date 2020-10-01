0204fbe5a168e0882db0c3f82edf5c27cc5f21fc
Usman Ahmad <usman.ahmad@7vals.com>
2020-10-01 19:08:00 +0500

    Finalized search UI

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,17 @@
-class SearchController < ApplicationController
-  # GET '/search', subdomain: /.+/
-  def search
-    @query = params[:query]
-    @query_regex = /#{@query}/i
+# frozen_string_literal: true
 
+class SearchController < ApplicationController
+  add_breadcrumb I18n.t('shared.home'), :root_path
+
+  # GET '/search'
+  def search
+    add_breadcrumb t('search.search')
+    @query = params[:query]
     return if @query.blank?
+
     @users    =  @current_company.users.accessible_by(current_ability, :read).where('users.name LIKE :query OR users.email LIKE :query', query: "%#{@query}%")
     @projects =  @current_company.projects.accessible_by(current_ability, :read).where('projects.name LIKE :name', name: "%#{@query}%")
-    @issues_pagy, @issues   =  pagy(@current_company.issues.accessible_by(current_ability, :read).where('issues.title LIKE :title', title: "%#{@query}%"))
+    @issues   =  @current_company.issues.accessible_by(current_ability, :read).where('issues.title LIKE :title', title: "%#{@query}%")
 
     respond_to do |format|
       format.html
