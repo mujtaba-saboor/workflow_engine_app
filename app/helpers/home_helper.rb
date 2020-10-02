64ffffdb4d506211b16557e506eb284e9dd016a7
Haider Shahid <haider.shahid@7vals.com>
2020-10-02 13:00:47 +0500

    partial added

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -11,22 +11,15 @@ module HomeHelper
   end
   
   def high_issues_percentage(user)
-    if user.staff?
-      issues = user.all_projects.joins(:issues)
-    else
-      issues = user.company.projects.joins(:issues)      
-    end
+    issues = user.all_issues
     total_issues = issues.count
-    high_priority_issues = issues.where(issues: { priority: 'high' }).count
+    high_priority_issues = issues.where(issues: { priority: Issue.priorities[:high] }).count
     high_priority_issues != 0 ? (high_priority_issues * 100) / total_issues : 0
   end
 
-  def issues_percentage(user)
-    if user.staff?
-      issues = user.all_projects.joins(:issues)
-    else
-      issues = user.company.projects.joins(:issues)      
-    end
+  def issues_percentage(user)    
+    
+    issues = user.all_issues
     total_issues = issues.count
 
     status_percentages = {}
